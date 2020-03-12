5b5f6a112360b662bf372aaac98c0831c5b208fb
kdog31 <kainenjw@gmail.com>
2020-03-12 04:54:24 +0000

    changes to internal repository updating

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ echo Creating environment file.
 echo DISCORD_TOKEN=$DISCORD_TOKEN\\nDISCORD_ID=$DISCORD_ID\\nBOT_NAME=$BOT_NAME\\nLOG_URL=$LOG_URL\\nPANIC_WORD=$PANIC_WORD\\nPANIC_LOG_LEN=$PANIC_LOG_LEN > /MegaBot/MegaBot/.env && echo Environment file created successfully
 cd /MegaBot/MegaBot
 echo Updating internal GIT repository
-git pull && echo Internal GIT repository up to date.
+git reset --hard HEAD && git pull && echo Internal GIT repository up to date.
 echo Launching bot.
 python3 bot.py
 echo Bot Exited.
