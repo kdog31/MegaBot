0df00d5f2cda687db2be09291d45f45b9c0530fe
kdog31 <kainenjw@gmail.com>
2020-03-15 05:36:13 +0000

    debugging

--- Cogs/Logger.py
+++ Cogs/Logger.py
@@ -139,8 +139,5 @@ class logging(commands.Cog):
     @commands.command()
     async def optout(self, ctx):
         print("optout triggered")
-        if not ctx.guild.id in self.optouts:
-            self.optouts[ctx.guild.id] = []
-        print(optouts)    
-        optouts[ctx.guild.id].append(ctx.author.id)
-        await ctx.send("{ctx.author}, You have successfully opted out of logging.")
+        print(ctx.guild.id)
+        print(optouts)
