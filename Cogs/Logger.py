3d4a3535e80c974aee0dfe4977266356c830eaca
kdog31 <kainenjw@gmail.com>
2020-03-15 06:09:13 +0000

    a

--- Cogs/Logger.py
+++ Cogs/Logger.py
@@ -79,7 +79,7 @@ class logging(commands.Cog):
         self.bot = bot
         self.optouts = {}
 
-    async def optcheck(ctx, self):
+    async def optcheck():
         try:
             if ctx.author.id in self.optouts[ctx.guild.id]:
                 return True
@@ -98,7 +98,7 @@ class logging(commands.Cog):
             
             dt_str = now.strftime("%m/%d/%Y-%H:%M:%S")
             
-            if not self.optcheck(ctx):
+            if not self.optcheck():
                 if os.path.exists("logs/{}/{}".format(guild, channel)):
                     open("logs/{}/{}/chat.log".format(guild, channel), "a").write(dt_str + ":" + user + ":" + message + "\n")
                 else:
