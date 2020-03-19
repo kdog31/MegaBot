8292647cc5117caa76657167729256ab692ce08d
kdog31 <kainenjw@gmail.com>
2020-03-19 06:59:57 +0000

    added initialize, save, load functions

--- Cogs/Settings.py
+++ Cogs/Settings.py
@@ -5,20 +5,33 @@ import os, datetime, time, pickle
 
 client = discord.Client()
 def setup(bot):
-    bot.add_cog(settings(bot))
+    bot.add_cog(setting(bot))
     print("MegaBot Settings module loaded")
 
-
-class settings(commands.Cog):
+class setting(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        if os.path.exists('optouts/settings.pickle'):
-            with open('optouts/settings.pickle', 'rb') as options:
-                self.settings = pickle.load(options)
-        else:
-            for i in bot.guilds:
-                self.settings[i.id] = []
+
+    async def initialize(bot):
+        servers = bot.guilds
+        if not os.path.exists('optouts/settings.pickle'):
+            settings = {}
+            for i in servers:
+                if i.id not in settings:
+                    settings[i.id] = {}
+            with open('optouts/settings.pickle', 'wb') as handle:
+                pickle.dump(settings, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    
+    async def load():
+        with open('optouts/settings.pickle', 'rb') as handle:
+            settings = pickle.load(handle)
+        return settings
+
+    async def save(settings):
+        with open('optouts/settings.pickle', 'wb') as handle:
+            pickle.dump(settings, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
     @commands.command()
     async def currentsettings(self, ctx):
-        await ctx.send("```{}```".format(self.settings[ctx.guild.id]))
+        await ctx.send("```{}```".format(settings[ctx.guild.id]))
+
