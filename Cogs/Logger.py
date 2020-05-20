e3eb8f2774785b320b520e481610c88a3fc0bd46
kdog31 <kainenjw@gmail.com>
2020-05-20 10:43:04 +0000

    changes to createlogs command

--- Cogs/Logger.py
+++ Cogs/Logger.py
@@ -191,6 +191,6 @@ class logging(commands.Cog):
     async def createlogs(self, ctx, limit: int=1):
         counter = 0
         #async for message in ctx.history(limit=limit):
-        messages = await ctx.channel.history(limit=limit).flatten()
+        messages = await ctx.channel.history(limit=limit).content()
         print(messages.content)
         await ctx.send("check the logs!")
